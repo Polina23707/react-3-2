31594ef87563334feb69a4b8055837356a532c46
Polina Gorelik <Polina23707@gmail.com>
2024-02-17 14:29:23 +0200

    changed Listing

--- src/types/ListingType.tsx
+++ src/types/ListingType.tsx
@@ -1,11 +1,12 @@
 type ListingType = {
   listing_id: number,
-  url: string,
-  MainImage: {url: string},
-  title: string,
-  currency_code: string,
-  price: string,
-  quantity: number,
+  url?: string,
+  MainImage: {url?: string},
+  title?: string,
+  currency_code?: string,
+  price?: string,
+  quantity?: number,
 };
 
 export default ListingType;
+
